596f3773167798bfdf93cbbea570416e665929b6
saugat21 <saugatbaral122@gmail.com>
2025-01-13 18:08:52 +0545

    generated schema

---
+++ src/utils/schemaGenerator.jsx
@@ -0,0 +1,21 @@
+const generateSchema = (components) => {
+  const schema = {};
+  components.forEach((component) => {
+    if (component.type === "text") {
+      schema[component.id] = z
+        .string()
+        .min(1, `${component.label} is required`);
+    } else if (component.type === "select") {
+      schema[component.id] = z
+        .string()
+        .min(1, `Please select ${component.label}`);
+    } else if (component.type === "radio") {
+      schema[component.id] = z
+        .string()
+        .min(1, `Please select an option for ${component.label}`);
+    }
+  });
+  return schema;
+};
+
+export default generateSchema;
